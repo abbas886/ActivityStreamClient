a254737f26d992ec80e424de70640d6d844820ea
abbas886 <abbas886@yahoo.com>
2017-05-30 10:48:22 +0530

    display users and non-users of particular circle

--- ActivityStream/WebContent/activity_stream_components/Circle/CircleService.js
+++ ActivityStream/WebContent/activity_stream_components/Circle/CircleService.js
@@ -15,6 +15,18 @@ app.factory('CircleService',['$http', '$rootScope', function($http,$rootScope){
         );
 	}
 	
+	circleService.getCircleUsers=function(circleName){
+		console.log('Calling getCircles')
+		return $http.get(BASE_URL + "/circleUsers/"+circleName)
+		.then(
+                function(response){
+                	/*$rootScope.circles=response.data*/
+                  return response.data;
+                }
+        );
+	}
+	
+	
 	circleService.createCircle=function(circle){
 		console.log('Calling createCircle')
 		return $http.post(BASE_URL + "/circle/create/",circle)
