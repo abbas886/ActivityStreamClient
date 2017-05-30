49da1a86f6cce90a00a5cf843de53b7cabd866a5
abbas886 <abbas886@yahoo.com>
2017-05-30 16:32:33 +0530

    Merge conflicts

--- ActivityStream/WebContent/activity_stream_components/Circle/CircleController.js
+++ ActivityStream/WebContent/activity_stream_components/Circle/CircleController.js
@@ -102,7 +102,7 @@ app
 											}
 									)
 						}
-<<<<<<< HEAD
+
 						
 						self.addUserToCircle = function(userID)
 						{
@@ -135,8 +135,8 @@ app
 									)
 							
 						}
-=======
->>>>>>> branch 'master' of https://github.com/abbas886/ActivityStreamClient.git
+
+
 							
 							
 						
